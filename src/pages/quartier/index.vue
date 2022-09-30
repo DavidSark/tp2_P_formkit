36c7dbf6a4260c39d8a44d40573d001fabbde84d
DavidSark <david.sarkissian@edu.univ-fcomte.fr>
2022-09-30 09:45:52 +0200

    Grouper les résultats

--- src/pages/quartier/index.vue
+++ src/pages/quartier/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import groupBy from "lodash/groupBy";
+import {Disclosure} from "@headlessui/vue";
 import { supabase } from "../../supabase";
 const { data, error } = await supabase.from("quartiercommune").select("*");
 if (error) console.log("n'a pas pu charger la table quartiercommune :", error);
@@ -8,12 +10,28 @@ if (error) console.log("n'a pas pu charger la table quartiercommune :", error);
 <template>
   <section class="flex flex-col">
     <h3 class="text-2xl">Liste des quartiers</h3>
-    <ul>
+    <!-- <ul>
       <li v-for="quartiercommune in (data as any[])">
         {{ quartiercommune.libelle_Commune }} -
         {{ quartiercommune.libelle_Quartier }}
       </li>
-    </ul>
+    </ul> -->
     
+    <Disclosure
+    v-for="(listeQuartier, libelle_Commune) in groupBy(
+      data,
+      'libelle_Commune'
+    )"
+    :key="libelle_Commune"
+    >
+    <DisclosureButton  v-for="c in (listeQuartier as any[])" :key="c.libelle_Commune"
+                class="border">
+        {{ c.libelle_Commune }}
+    </DisclosureButton>
+
+    <DisclosurePanel  v-for="q in (listeQuartier as any[])" :key="q.libelle_Quartier">
+        {{ q.libelle_Quartier }}
+    </DisclosurePanel>
+    </Disclosure>
   </section>
 </template>
